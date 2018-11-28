41a7cae141a9584b12dff1249c7642817ea74bc8
Unknown <slepzs@gmail.com>
2018-11-28 11:31:26 +0100

    Auto stash before merge of "master" and "origin/master"

--- Project4/Project4/Master.Master.cs
+++ Project4/Project4/Master.Master.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Project4
 {
-    public partial class Index : System.Web.UI.Page
+    public partial class Master : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
