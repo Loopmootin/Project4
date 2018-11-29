cf7dd3e9df12580ef64feb6a8e71dcf1720cfc2e
Loopmootin <christoffera@outlook.dk>
2018-11-29 21:57:24 +0100

    Revert "Auto stash before merge of "master" and "origin/master""
    This reverts commit 58631f8f0c6111587c48bb134ca54ebd8f4a59ad.

--- Project4/Project4/custom.js
+++ Project4/Project4/custom.js
@@ -1,10 +1,13 @@
 ﻿$(document).ready(function () {
-        $('#search').hover(
-            function () {
-                $(this).animate({ width: '150px' }, 500);
-            },
-            function () {
-                    $(this).animate({ width: '100px' }, 300);             
-            }
-        );
+
+
+    $('#search').hover(
+        function () {
+            $(this).animate({ width: '150px' }, 500);
+        },
+        function () {
+            $(this).animate({ width: '100px' }, 300);
+        }
+    );
+
 });
