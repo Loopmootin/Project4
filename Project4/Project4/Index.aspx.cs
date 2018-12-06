eafba23f383f4ef94c663835366e4d3536421ebb
Unknown <slepzs@gmail.com>
2018-12-06 12:12:50 +0100

    Slettet Adminpage

--- Project4/Project4/Index.aspx.cs
+++ Project4/Project4/Index.aspx.cs
@@ -5,6 +5,10 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Xml.Xsl;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -14,6 +18,23 @@ namespace Project4
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // if the XML uses a namespace, the XSLT must refer to this namespace
+            string sourcefile = Server.MapPath("xml/Commercials.xml");
+            string xslfile = Server.MapPath("xml/CommercialsXSLT.xslt");
+            string destinationfile = Server.MapPath("xml/CommercialsTransformed.xml");
+
+            FileStream fs = new FileStream(destinationfile, FileMode.Create);
+            XslCompiledTransform xct = new XslCompiledTransform();
+
+            xct.Load(xslfile);
+            xct.Transform(sourcefile, null, fs);
+            fs.Close();
+
+            DataSet ds = new DataSet();
+            ds.ReadXml(destinationfile);
+            DataTable dt = ds.Tables[0];
+
+
             Utility connect = new Utility();
 
             try
