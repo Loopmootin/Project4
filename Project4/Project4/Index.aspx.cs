20269d0af28b4720c67c80b48584f2e397a915c9
Loopmootin <christoffera@outlook.dk>
2018-12-11 15:56:58 +0100

    commercial style

--- Project4/Project4/Index.aspx.cs
+++ Project4/Project4/Index.aspx.cs
@@ -27,7 +27,7 @@ namespace Project4
             DataSet ds = new DataSet();
             ds.ReadXml(destinationfile);
             DataTable dt = ds.Tables[0];
-
+                
             int viewcounter = 0;
             int randomCommercial = (new Random()).Next(0, dt.Rows.Count);
 
@@ -35,10 +35,7 @@ namespace Project4
 
             dt.Rows[randomCommercial][3] = viewcounter;
 
-            CommercialPoster.ImageUrl = Convert.ToString(dt.Rows[randomCommercial][1]);
-
-            //GridView1.DataSource = dt;
-            //GridView1.DataBind();
+            CommercialPoster.ImageUrl = "pictures/" + Convert.ToString(dt.Rows[randomCommercial][1]);
 
             ds.WriteXml(Server.MapPath("xml/CommercialsTransformed.xml"));
 
--- Project4/Project4/Index.aspx.designer.cs
+++ Project4/Project4/Index.aspx.designer.cs
@@ -13,22 +13,13 @@ namespace Project4 {
     public partial class Index {
         
         /// <summary>
-        /// GridView1 control.
+        /// CommercialPoster control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
-        
-        /// <summary>
-        /// Image1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image1;
+        protected global::System.Web.UI.WebControls.Image CommercialPoster;
         
         /// <summary>
         /// RepeaterTop control.
